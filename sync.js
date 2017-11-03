3a6231a5f88bea294b5055e6b378d3d09c8c8b6f
tony <tonyofbyteball@users.noreply.github.com>
2017-11-03 18:54:57 +0300

    sync script

---
+++ sync.js
@@ -0,0 +1,37 @@
+/*jslint node: true */
+'use strict';
+const conf = require('byteballcore/conf');
+const db = require('byteballcore/db');
+const eventBus = require('byteballcore/event_bus');
+const headlessWallet = require('headless-byteball');
+
+
+headlessWallet.setupChatEventHandlers();
+
+
+eventBus.on('paired', from_address => {
+	let device = require('byteballcore/device.js');
+	device.sendMessageToDevice(from_address, 'text', "The bot is in maintenance mode, please check again later.");
+});
+
+eventBus.on('text', (from_address, text) => {
+	let device = require('byteballcore/device.js');
+	device.sendMessageToDevice(from_address, 'text', "The bot is in maintenance mode, please check again later.");
+});
+
+
+eventBus.on('headless_wallet_ready', () => {
+	let error = '';
+	let arrTableNames = ['users', 'receiving_addresses', 'transactions'];
+	db.query("SELECT name FROM sqlite_master WHERE type='table' AND name IN (?)", [arrTableNames], (rows) => {
+		if (rows.length !== arrTableNames.length) error += texts.errorInitSql();
+
+		if (conf.useSmtp && (!conf.smtpUser || !conf.smtpPassword || !conf.smtpHost)) error += texts.errorSmtp();
+
+		if (!conf.admin_email || !conf.from_email) error += texts.errorEmail();
+
+		if (error)
+			throw new Error(error);
+
+	});
+});
