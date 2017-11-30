182d0511fa26016ef349cca36a37641eba72d722
tony <tonyofbyteball@users.noreply.github.com>
2017-11-30 17:04:30 +0300

    read myAddress before issuing

--- scripts/issue_tokens.js
+++ scripts/issue_tokens.js
@@ -27,24 +27,6 @@ function updateAssetInConf(issued_asset){
 }
 
 function checkAndIssue(){
-	if (!conf.issued_asset)
-		return defineAsset();
-	db.query("SELECT is_stable FROM assets JOIN units USING(unit) WHERE unit=?", [conf.issued_asset], function(rows){
-		if (rows.length === 0)
-			throw Error("asset "+conf.issued_asset+" not found");
-		if (rows[0].is_stable === 0){
-			console.error('==== already defined but the definition is not stable yet, will wait for stability and issue');
-			return waitForStabilityAndIssue();
-		}
-		db.query("SELECT 1 FROM outputs WHERE asset=? LIMIT 1", [conf.issued_asset], function(rows){
-			if (rows.length > 0)
-				return console.error('==== already issued');
-			issueAsset();
-		});
-	});
-}
-
-function defineAsset() {
 	db.query(
 		"SELECT address, SUM(amount) AS amount FROM my_addresses CROSS JOIN outputs USING(address) JOIN units USING(unit) \n\
 		WHERE is_spent=0 AND asset IS NULL AND is_stable=1 GROUP BY address", 
@@ -61,22 +43,40 @@ function defineAsset() {
 				});
 			}
 
-			const composer = require('byteballcore/composer.js');
-			const network = require('byteballcore/network');
-			let callbacks = composer.getSavingCallbacks({
-				ifNotEnoughFunds: onError,
-				ifError: onError,
-				ifOk: objJoint => {
-					network.broadcastJoint(objJoint);
-					conf.issued_asset = objJoint.unit.unit;
-					updateAssetInConf(conf.issued_asset);
-					console.error("==== Defined asset, now waiting for stability of asset definition unit " + conf.issued_asset);
-					waitForStabilityAndIssue();
+			if (!conf.issued_asset)
+				return defineAsset();
+			db.query("SELECT is_stable FROM assets JOIN units USING(unit) WHERE unit=?", [conf.issued_asset], function(rows){
+				if (rows.length === 0)
+					throw Error("asset "+conf.issued_asset+" not found");
+				if (rows[0].is_stable === 0){
+					console.error('==== already defined but the definition is not stable yet, will wait for stability and issue');
+					return waitForStabilityAndIssue();
 				}
+				db.query("SELECT 1 FROM outputs WHERE asset=? LIMIT 1", [conf.issued_asset], function(rows){
+					if (rows.length > 0)
+						return console.error('==== already issued');
+					issueAsset();
+				});
 			});
-			composer.composeAssetDefinitionJoint(myAddress, conf.asset_definition, headlessWallet.signer, callbacks);
 		}
 	);
+}
+
+function defineAsset() {
+	const composer = require('byteballcore/composer.js');
+	const network = require('byteballcore/network');
+	let callbacks = composer.getSavingCallbacks({
+		ifNotEnoughFunds: onError,
+		ifError: onError,
+		ifOk: objJoint => {
+			network.broadcastJoint(objJoint);
+			conf.issued_asset = objJoint.unit.unit;
+			updateAssetInConf(conf.issued_asset);
+			console.error("==== Defined asset, now waiting for stability of asset definition unit " + conf.issued_asset);
+			waitForStabilityAndIssue();
+		}
+	});
+	composer.composeAssetDefinitionJoint(myAddress, conf.asset_definition, headlessWallet.signer, callbacks);
 }
 
 function issueAsset(){
