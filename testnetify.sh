d677716bbe71bcb492546cefd68187e47f94501f
tony <tonyofbyteball@users.noreply.github.com>
2019-06-09 00:25:54 +0300

    .env

--- testnetify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sed -ie "s/version = '1.0'/version = '1.0t'/; s/alt = '1'/alt = '2'/" node_modules/ocore/constants.js
-sed -ie "s/.hub = 'obyte.org\/bb'/.hub = 'obyte.org\/bb-test'/" conf.js
